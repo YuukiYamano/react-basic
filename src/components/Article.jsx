67651f792a5056d13ff76adabbb09c47d205090c
YuukiYamano <yuukiyamano1108@gmail.com>
2023-01-30 22:03:22 +0900

    feat : use state

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,17 @@
-import {Content, Title} from './index'
+import { useState } from 'react';
+import {Content, Title,PublishButton} from './index'
 
 const Article = (props)=>{
+    const [isPublished,setIsPublished] = useState(false)
+    const publishArticle = () =>{
+        console.log(1111111);
+        setIsPublished(true)
+    }
     return(
        <div>
         <Title title={props.title}/>
         <Content content={props.content}/>
+        <PublishButton isPublished = {isPublished} onClick={publishArticle}/>
        </div>
     );
 };
